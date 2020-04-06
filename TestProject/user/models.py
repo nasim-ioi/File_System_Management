96c8fda496a4c425710caec8aab28eae633f60d2
nasim <rezaie.nasim.imo@gmail.com>
2020-04-07 00:41:40 +0430

    alter profile model

--- TestProject/user/models.py
+++ TestProject/user/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profiles')
-    is_owner = models.BooleanField()
+    is_owner = models.BooleanField(default=False)
     phone_number = models.CharField(max_length = 50, null = True , blank = True)
 
     def __str__(self):
