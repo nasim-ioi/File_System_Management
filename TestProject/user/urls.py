7c116d7639f8dc2081deac681d2908384d44b98e
nasim <rezaie.nasim.imo@gmail.com>
2020-04-07 00:42:08 +0430

    alter url name

--- TestProject/user/urls.py
+++ TestProject/user/urls.py
@@ -5,8 +5,8 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from .views import Signup
 
 urlpatterns = [
-    path('', obtain_jwt_token),
-    path('login/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
+    path('', obtain_jwt_token, name='redirect_to_obtain_jwt_token'),
+    path('login/', obtain_jwt_token, name='obtain_jwt_token'),
+    path('api-token-refresh/', refresh_jwt_token, name='refresh_jwt_token'),
     path('signup/', Signup.as_view(), name='signup'),
 ]
